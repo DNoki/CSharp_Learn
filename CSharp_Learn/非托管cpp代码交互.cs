9f5fb1a26c6efd3066e1a52c2bd4ef6088789ba6
DNoki <490889409@qq.com>
2021-01-07 19:21:05 +0900

    C++交互学习

---
+++ CSharp_Learn/非托管cpp代码交互.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using System.Text;
+
+
+/// <summary>
+/// 与非托管代码交互操作
+/// </summary>
+public class InteroperationWithUnmanagedCode
+{
+    public const string DLL_PATH = "../../../../x64/Debug/Cpp_Learn.dll";
+
+    /// <summary>
+    /// 加载动态库
+    /// </summary>
+    /// <param name="lpFileName"></param>
+    /// <returns></returns>
+    [DllImport("kernel32.dll")]
+    private static extern IntPtr LoadLibrary(string lpFileName);
+    /// <summary>
+    /// 获取函数地址
+    /// </summary>
+    /// <param name="hModule"></param>
+    /// <param name="lpProcName"></param>
+    /// <returns></returns>
+    [DllImport("kernel32.dll")]
+    private static extern IntPtr GetProcAddress(IntPtr hModule, string lpProcName);
+    /// <summary>
+    /// 释放动态库
+    /// </summary>
+    /// <param name="hModule"></param>
+    /// <returns></returns>
+    [DllImport("kernel32.dll")]
+    private static extern bool FreeLibrary(IntPtr hModule);
+
+
+    /// <summary>
+    /// 对话框的内容和行为。
+    /// </summary>
+    public enum MsgBoxCheckFlags : uint
+    {
+        MB_OK = 0x00000000u,
+        MB_OKCANCEL,
+        MB_ABORTRETRYIGNORE,
+        MB_YESNOCANCEL,
+        MB_YESNO,
+        MB_RETRYCANCEL,
+        MB_CANCELTRYCONTINUE,
+    }
+    /// <summary>
+    /// 显示一个模式对话框，其中包含系统图标，一组按钮以及特定于应用程序的简短消息，例如状态或错误信息。该消息框返回一个整数值，该整数值指示用户单击了哪个按钮。
+    /// https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-messagebox
+    /// </summary>
+    /// <param name="hWnd"></param>
+    /// <param name="lpText"></param>
+    /// <param name="lpCaption"></param>
+    /// <param name="uType"></param>
+    /// <returns></returns>
+    [DllImport("User32.dll", EntryPoint = "MessageBox", CharSet = CharSet.Auto)]
+    private static extern int MessageBox(IntPtr hWnd, string lpText, string lpCaption, MsgBoxCheckFlags uType);
+
+    /// <summary>
+    /// DllImport声明动态库中的函数
+    /// </summary>
+    [DllImport(DLL_PATH)]
+    private static extern void SampleCFunction();
+    /// <summary>
+    /// 定义 EntryPoint 可以重命名函数
+    /// </summary>
+    [DllImport(DLL_PATH, EntryPoint = "SampleFunction")]
+    private static extern void SampleFunctionChangedName();
+
+    [DllImport(DLL_PATH)]
+    private static extern void SampleBaseType(
+        IntPtr _void_P,
+        short _short,
+        ushort _ushort,
+        int _int,
+        uint _uint,
+        long _long,
+        ulong _ulong,
+        bool _bool,
+        float _float,
+        double _double,
+        char _char,
+        byte _uchar,
+        char _wchar_t,
+        string _char_P,
+        string _C_char_P,
+        string _wchar_t_P,
+        string _C_wchar_t_P
+        );
+
+
+    public static void TestRun()
+    {
+        // 调用 WindowsAPI User32.dll 的 MsgBox 函数
+        //var result = MessageBox(IntPtr.Zero, "Text", "Caption", MsgBoxCheckFlags.MB_YESNOCANCEL);
+        //Console.WriteLine(result);
+
+        SampleCFunction();
+        SampleFunctionChangedName();
+
+        // 动态加载dll
+        {
+            var dllPtr = LoadLibrary(DLL_PATH);
+            var funPtr = GetProcAddress(dllPtr, "SampleFunction");
+
+            var action = Marshal.GetDelegateForFunctionPointer<Action>(funPtr);
+            action.Invoke();
+
+            Console.WriteLine($"释放dll：{FreeLibrary(dllPtr)}");
+        }
+
+        SampleBaseType(
+            IntPtr.MaxValue,
+            short.MaxValue,
+            ushort.MaxValue,
+            int.MaxValue,
+            uint.MaxValue,
+            long.MaxValue,
+            ulong.MaxValue,
+            default(bool),
+            float.MaxValue,
+            double.MaxValue,
+            char.MaxValue,
+            byte.MaxValue,
+            char.MaxValue,
+            "_char_P",
+            "_C_char_P",
+            "_wchar_t_P",
+            "_C_wchar_t_P"
+            );
+    }
+}
---
+++ Cpp_Learn/dllmain.cpp
@@ -0,0 +1,76 @@
+﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
+#include "pch.h"
+
+#pragma execution_character_set("utf-8")
+
+#include <iostream>
+
+using namespace std;
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+{
+    switch (ul_reason_for_call)
+    {
+    case DLL_PROCESS_ATTACH:
+    case DLL_THREAD_ATTACH:
+    case DLL_THREAD_DETACH:
+    case DLL_PROCESS_DETACH:
+        break;
+    }
+    return TRUE;
+}
+
+// 以C语言接口的方式导出
+extern "C" _declspec(dllexport) void SampleCFunction()
+{
+    cout << "执行 void SampleCFunction()" << endl;
+}
+
+// 以模块定义文件的方式导出
+// 在Source.def文件的 EXPORTS 下，添加该函数
+void SampleFunction()
+{
+    cout << "执行 void SampleFunction()" << endl;
+}
+
+// 平台调用数据类型
+// https://docs.microsoft.com/zh-cn/previous-versions/dotnet/netframework-4.0/ac7ay120(v=vs.100)
+void SampleBaseType(
+    void* _void_P,
+    short _short,
+    unsigned short _ushort,
+    int _int,
+    unsigned int _uint,
+    long _long,
+    unsigned long _ulong,
+    bool _bool,
+    float _float,
+    double _double,
+    char _char,
+    unsigned char _uchar,
+    wchar_t _wchar_t,
+    char* _char_P,
+    const char* _C_char_P,
+    wchar_t* _wchar_t_P,
+    const wchar_t* _C_wchar_t_P
+    )
+{
+    cout << "SampleBaseType()" << endl;
+    cout << _void_P << endl;
+    cout << _short << endl;
+    cout << _ushort << endl;
+    cout << _int << endl;
+    cout << _uint << endl;
+    cout << _long << endl;
+    cout << _ulong << endl;
+    cout << _bool << endl;
+    cout << _float << endl;
+    cout << _double << endl;
+    cout << _char << endl;
+    cout << _uchar << endl;
+    cout << _wchar_t << endl;
+    cout << _char_P << endl;
+    cout << _C_char_P << endl;
+    cout << _wchar_t_P << endl;
+    cout << _C_wchar_t_P << endl;
+}
