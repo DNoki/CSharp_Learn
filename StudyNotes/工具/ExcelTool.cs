bfe2cb9db107a8a18f8ce8f1abce2a415f0ad4d1
DNoki <490889409@qq.com>
2021-03-24 09:27:25 +0900

    添加 Excel工具

---
+++ StudyNotes/工具/ExcelTool.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NPOI;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+
+namespace StudyNotes.工具
+{
+    public class ExcelTool
+    {
+        public static void RadeExcel(string path)
+        {
+            var daTable = new DataTable();
+            var rowList = new List<string>();
+            using (var stream = new FileStream(path, FileMode.Open))
+            {
+                stream.Position = 0;
+                var xssWorkbook = new XSSFWorkbook(stream);
+            }
+        }
+    }
+}
